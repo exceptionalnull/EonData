299108695112c1dea906e340261c1d2caab3a0ba
Mike D <mike@eondata.net>
2023-03-02 21:14:01 +0800

    added iam policy for s3 access for api hosts

--- AWS/Scripts/deploy.sh
+++ AWS/Scripts/deploy.sh
@@ -2,4 +2,5 @@
 echo "Uploading angular webapp to S3 bucket..."
 aws s3 sync ~/projects/EonData/EonData.Web/dist/eon-data.web/ s3://eondataweb/
 echo "Uploading web api to S3 bucket..."
-aws s3 sync ~/projects/EonData/EonData.Api/bin/Release/net7.0/ s3://eondata/bin/eondata-api/
+tar -czvf eondataweb-api.tar.gz -C ~/projects/EonData/EonData.Api/bin/Release/net7.0 .
+aws s3 sync eondataweb-api.tar.gz s3://eondata/bin/eondataweb-api.tar.gz
