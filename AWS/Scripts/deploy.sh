fa58ca7a7ce10953c02f48087207f611fc7401b4
Mike D <mike@eondata.net>
2023-03-04 19:22:54 +0800

    fixed api s3 iam policies. improved deployment script.

--- AWS/Scripts/deploy.sh
+++ AWS/Scripts/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Uploading angular webapp to S3 bucket..."
-aws s3 sync ~/projects/EonData/EonData.Web/dist/eon-data.web/ s3://eondataweb/
+aws s3 sync ~/projects/EonData/EonData.Web/dist/eon-data.web/ s3://eondataweb/ --delete --exact-timestamps
 echo "Uploading web api to S3 bucket..."
-tar -czvf eondataweb-api.tar.gz -C ~/projects/EonData/EonData.Api/bin/Release/net7.0 .
-aws s3 sync eondataweb-api.tar.gz s3://eondata/bin/eondataweb-api.tar.gz
+aws s3 sync ~/projects/EonData/EonData.Api/bin/Publish/Release/net7.0/linux-x64/ s3://eondataweb-data/bin/api/ --delete --exact-timestamps
