35713202c66c5ad3ff17e07d255e0cc28b1d2277
Mike D <mike@eondata.net>
2023-03-17 00:12:10 +0800

    updated web api template to configure and deploy the dotnet app and nginx

--- EonData.Api/Program.cs
+++ EonData.Api/Program.cs
@@ -1,12 +1,26 @@
 using Amazon.S3;
 using EonData.CloudControl.AWS;
 
+
+const string socketFile = "/run/eondata-api.sock";
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// configure AWS services
+builder.WebHost
+    // use unix socket file
+    .UseSockets()
+    .ConfigureKestrel((context, serverOptions) => {
+        serverOptions.ListenUnixSocket(socketFile);
+    });
+
 builder.Services
+    // improved systemd integration
+    .AddSystemd()
+    // add AWS services
     .AddAWSService<IAmazonS3>()
     .AddScoped<S3FileStorageService>();
+
 
 var app = builder.Build();
 app.Run();
