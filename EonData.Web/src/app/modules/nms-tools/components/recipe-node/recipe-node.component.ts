174856040c9519c2730de7e5b25bd71a4df2d305
Mike D <mike@eondata.net>
2023-10-19 12:22:26 +0800

    working on recursive calling of node component

--- EonData.Web/src/app/modules/nms-tools/components/recipe-node/recipe-node.component.ts
+++ EonData.Web/src/app/modules/nms-tools/components/recipe-node/recipe-node.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Item } from '../../models/item.interface';
 import { NmsDataService } from '../../services/nms-data.service';
 import { Recipe } from '../../models/recipe.interface';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'nms-recipe-node',
@@ -13,14 +15,22 @@ export class RecipeNodeComponent implements OnInit {
   itemInfo: Item | undefined;
   recipes: Recipe[] | undefined;
   selectedRecipe: number = 0;
+  isIntermediate: boolean = false;
+  ingredients: Observable<number[]> | undefined;
 
   constructor(private dataService: NmsDataService) { }
 
   ngOnInit() {
     this.itemInfo = this.dataService.getItem(this.itemId);
     this.recipes = this.dataService.getRecipesByItem(this.itemId);
+
+    if (this.recipes !== undefined) {
+      this.selectedRecipe = this.recipes[0].recipeId;
+      this.isIntermediate = true;
+    }
   }
 
   pickedRecipe() {
+
   }
 }
