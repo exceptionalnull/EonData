d815da7aacbd220948fa41620c8b157a96890139
Mike D <mike@eondata.net>
2023-12-10 14:01:33 +0800

    updated version numbers

--- EonData.Web/src/environments/constants.ts
+++ EonData.Web/src/environments/constants.ts
@@ -2,5 +2,5 @@ import packageJson from '../../package.json';
 
 export class EnvironmentConstants {
   static readonly appVersion: string = packageJson.version;
-  static readonly apiVersion: string = "0.1.0.0";
+  static readonly apiVersion: string = "0.1.0.1";
 }
